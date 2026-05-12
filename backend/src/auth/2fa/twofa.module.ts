12586211ef3ec571531110c535916e129e2dc7ae
yutisio12 <yutisiogamings@gmail.com>
2026-05-12 19:17:30 +0700

    BE:
    - rectifying twofa module

--- backend/src/auth/2fa/twofa.module.ts
+++ backend/src/auth/2fa/twofa.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TwoFaService } from './twofa.service';
 import { TwoFaController } from './twofa.controller';
 
 @Module({
+  imports: [ConfigModule],
   providers: [TwoFaService],
   controllers: [TwoFaController],
 })
